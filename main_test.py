6be1c756b5acd01aac730828d6646d7cc65f2346
Andres Silva <Andres.Silva@anuntatech.com>
2022-08-21 15:06:46 -0500

    Revert "Test fail"
    This reverts commit 181f51ea6cd10e4ba2a0a81e3bb3100b1cb964eb.

--- main_test.py
+++ main_test.py
@@ -6,7 +6,7 @@ client = TestClient(app)
 
 def test_route():
     response = client.get('/roleUsers/337c929384d461e87dd550e15a8d6ff7c6878247')
-    assert response.status_code == 400
+    assert response.status_code == 200
 
 def test_route_no_content():
     response = client.get('/roleUsers/test')
