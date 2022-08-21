fa13d476dc9884a1ac5db9b6ffbaea81e9bb1b9e
Andres Silva <Andres.Silva@anuntatech.com>
2022-08-21 05:44:24 -0500

    Funcionalidad necesaria + tests

--- main.py
+++ main.py
@@ -1,22 +1,35 @@
-from typing import Union
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Response
 import requests
 from prometheus_fastapi_instrumentator import Instrumentator
 import logging.config
+from pydantic import BaseModel
+import datetime
 
 # setup loggers
 logging.config.fileConfig('logging.conf', disable_existing_loggers=False)
 
+logger = logging.getLogger(__name__)
+
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    url = 'https://62f6640ba3bce3eed7c04b72.mockapi.io/items'
-    response = requests.get(url, {}, timeout=5)
-    return {"items": response.json() }
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
+class ResponseData(BaseModel):
+    encryptedToken: str 
+    role: str = ""
+    expirationDate: datetime.date = datetime.date.today().strftime("%d/%m/%Y")
 
 Instrumentator().instrument(app).expose(app)
+
+@app.get("/roleUsers/{encryptedToken}")
+def read_root(encryptedToken: str):
+    logger.debug("encryptedToken recibido: " + encryptedToken)
+    
+    url = 'https://63016ffbe71700618a3866e4.mockapi.io/users'
+    responseData = ResponseData(encryptedToken= encryptedToken)
+    
+    requestResult = requests.get(url + "?encryptedToken=" + encryptedToken, timeout=5)
+    
+    if(len(requestResult.json()) >= 1):
+        responseData.encryptedToken = requestResult.json()[0]["role"]
+        return Response(content= responseData.json())
+    else:
+        return Response(status_code= status.HTTP_204_NO_CONTENT)
--- main_test.py
+++ main_test.py
@@ -4,6 +4,10 @@ from main import app
 
 client = TestClient(app)
 
-def test_index_route():
-    response = client.get('/items/1')
+def test_route():
+    response = client.get('/roleUsers/0da37474320000bfb14d1cbeb8a508687c5053b0')
     assert response.status_code == 200
+
+def test_route_no_content():
+    response = client.get('/roleUsers/test')
+    assert response.status_code == 204
